2cc8dbbcd010de7ded4db7c1e6a52d9ec9bea674
Steve987321 <Stevebudding123@gmail.com>
2024-06-25 00:12:12 +0200

    Improved chest stealer

--- ToadClient/src/Toad/Modules/chest_stealer.cpp
+++ ToadClient/src/Toad/Modules/chest_stealer.cpp
@@ -7,6 +7,16 @@ using namespace toad;
 namespace toadll
 {
 
+CChestStealer::CChestStealer()
+{
+	int index = 0;
+	for (int j = chest_stealer::begin_y, sloty = 0; sloty < 3; j += 35, sloty++)
+		for (int i = chest_stealer::begin_x, slotx = 0; slotx < 9; i += 35, slotx++)
+		{
+			m_slotToMousePosOffset[index++] = { i, j };
+		}
+}
+
 void CChestStealer::PreUpdate()
 {
 	WaitIsVerified();
@@ -23,54 +33,141 @@ void CChestStealer::Update(const std::shared_ptr<LocalPlayer>& lPlayer)
 		return;
 	}
 
-	static bool chest_open = false;
+	static bool looting = false;
+	static int index = -1;
 
-	if (GetAsyncKeyState(82) & 1 /*(GetAsyncKeyState(VK_RBUTTON) & 1) && !GetAsyncKeyState(VK_SHIFT) && !chest_open*/)
-	{
-		std::array<std::string, 27> contents = MC->GetChestContents();
+	static Timer aim_timer;
+	static POINT current_pos{};
 
-		if (contents.empty())
+	if (CVarsUpdater::IsInGui && GetAsyncKeyState(82) & 1 || looting /*(GetAsyncKeyState(VK_RBUTTON) & 1) && !GetAsyncKeyState(VK_SHIFT) && !chest_open*/)
+	{		
+		if (!looting)
 		{
-			SLEEP(100);
-			return;
+			{
+				std::lock_guard lock(m_mutex);
+				m_chestContents = MC->GetChestContents();
+
+				if (m_chestContents.empty())
+				{
+					SLEEP(100);
+					return;
+				}
+
+			}
+
+			// setup mouse path
+			SetupPath();
+
+			// get the first index from the path
+			index = m_indexPath.front();
+			m_indexPath.pop_front();
+		
+			// start holding shift and wait a bit
+			send_key(VK_SHIFT);
+			m_isShiftDown = true;
+			SLEEP(rand_int(30, 50));
+
+			// start timer and set starting mouse position
+			aim_timer.Start();
+			GetCursorPos(&current_pos);
+			looting = true;
 		}
 
-		send_key(VK_SHIFT);
-
-		SLEEP(100);
-
-		const POINT middle = get_middle_of_screen();
-		for (int i = 0; i < 27; i++)
+		if (index != -1)
 		{
-			const std::string& name = contents[i];
-			if (name.empty())
-				continue;
+			if (GetAsyncKeyState(0x45 /*E*/) || GetAsyncKeyState(VK_ESCAPE))
+			{
+				index = -1;
+				return;
+			}
 
-			POINT pos = m_slotToMousePosOffset[i];
+			const std::string& name = m_chestContents[index];
+
+			POINT pos = m_slotToMousePosOffset[index];
+			const POINT middle = get_middle_of_screen();
 
 			pos.x += middle.x;
 			pos.y += middle.y;
 
-			
-			SetCursorPos(pos.x, pos.y);
-			SLEEP(50);
-			right_mouse_down(pos);
-			SLEEP(10);
-			right_mouse_up(pos);
-			SLEEP(300);
+			if (aim_timer.Elapsed() > chest_stealer::average_slowness_ms)
+			{
+				// make sure mouse is on slot and right click
+				SetCursorPos(pos.x, pos.y);
+				SLEEP(rand_int(30, 70));
+
+				right_mouse_down(pos);
+				SLEEP(rand_int(50, 100));
+				right_mouse_up(pos);
+
+				current_pos = pos;
+
+				// get next in the path
+				index = m_indexPath.front();
+				m_indexPath.pop_front();
+
+				// restart timer
+				aim_timer.Start();
+			}
+			else
+			{
+				float t = std::clamp(aim_timer.Elapsed() / chest_stealer::average_slowness_ms, 0.f, 1.f);
+				int x_lerp = (int)slerp(current_pos.x, pos.x, t);
+				int y_lerp = (int)slerp(current_pos.y, pos.y, t);
+				SetCursorPos(x_lerp, y_lerp);
+			}
+		}
+		else
+		{
+			if (m_isShiftDown)
+			{
+				send_key(VK_SHIFT, false);
+				m_isShiftDown = false;
+			}
+
+			looting = false;
+		}
+	}
+	else
+	{
+		if (m_isShiftDown)
+		{
+			send_key(VK_SHIFT, false);
+			m_isShiftDown = false;
+		}
+		looting = false;
+	}
+
+	if (index == -1)
+	{
+		if (m_isShiftDown)
+		{
+			send_key(VK_SHIFT, false);
+			m_isShiftDown = false;
+		}
+		looting = false;
+	}
+}
+
+void CChestStealer::SetupPath()
+{
+	m_indexPath.clear();
+
+	for (int i = 0; i < m_chestContents.size(); i++)
+	{
+		const std::string& name = m_chestContents[i];
+		if (name.empty())
+		{
+			continue;
 		}
 
-		send_key(VK_SHIFT, false);
-
-		chest_open = true;
+		m_indexPath.emplace_back(i);
 	}
 
-	if (chest_open && (GetAsyncKeyState(VK_ESCAPE) || GetAsyncKeyState(0x45 /*E*/)))
-	{
-		chest_open = false;
-	}
+	// for now it just randomizes the path
+	static auto rng = std::default_random_engine{};
+	std::ranges::shuffle(m_indexPath, rng);
 
-	get_middle_of_screen();
+	m_indexPath.emplace_back(-1);
 }
 
 POINT CChestStealer::get_middle_of_screen() const
@@ -87,17 +184,6 @@ POINT CChestStealer::get_middle_of_screen() const
 	return pt;
 }
 
-CChestStealer::CChestStealer()
-{
-	int index = 0;
-	for (int j = chest_stealer::begin_y, sloty = 0; sloty < 3; j += 35, sloty++)
-		for (int i = chest_stealer::begin_x, slotx = 0; slotx < 9; i += 35, slotx++)
-		{
-			LOGDEBUG("i:{} \t j:{} \t index:{}", i, j, index);
-			m_slotToMousePosOffset[index++] = { i, j };
-		}
-}
-
 void CChestStealer::right_mouse_down(const POINT& pt)
 {
 	PostMessage(g_hWnd, WM_RBUTTONDOWN, MKF_RIGHTBUTTONDOWN, LPARAM((pt.x, pt.y)));
@@ -108,26 +194,56 @@ void CChestStealer::right_mouse_up(const POINT& pt)
 	PostMessage(g_hWnd, WM_RBUTTONUP, 0, LPARAM((pt.x, pt.y)));
 }
 
-void CChestStealer::OnImGuiRender(ImDrawList* draw)
-{
-	if (chest_stealer::show_grid)
-	{
-		ImVec2 middle = ImGui::GetMainViewport()->Size;
-		middle.x /= 2;
-		middle.y /= 2;
-		middle.x -= 10;
-		middle.y -= 50;
-
-		for (int i = 0; i < 27; i++)
-		{
-			POINT pos = m_slotToMousePosOffset[i];
-
-			pos.x += (int)middle.x;
-			pos.y += (int)middle.y;
-
-			draw->AddText({(float)pos.x, (float)pos.y}, IM_COL32(255, 0, 0, 255), std::to_string(i).c_str());
-		}
-	}
-}
+//void CChestStealer::OnImGuiRender(ImDrawList* draw)
+//{
+//	if (chest_stealer::show_slot_positions)
+//	{
+//		ImVec2 middle = ImGui::GetMainViewport()->Size;
+//		middle.x /= 2;
+//		middle.y /= 2;
+//		middle.x -= 10;
+//		middle.y -= 50;
+//
+//		for (int i = 0; i < 27; i++)
+//		{
+//			POINT pos = m_slotToMousePosOffset[i];
+//
+//			pos.x += (int)middle.x;
+//			pos.y += (int)middle.y;
+//
+//			draw->AddText({(float)pos.x, (float)pos.y}, IM_COL32(255, 0, 0, 255), std::to_string(i).c_str());
+//		}
+//	}
+//
+//	else if (chest_stealer::show_info)
+//	{
+//		ImVec2 middle = ImGui::GetMainViewport()->Size;
+//
+//		middle.x /= 2;
+//		middle.y /= 2;
+//		middle.x -= 10;
+//		middle.y -= 50;
+//
+//		for (int i = 0; i < 27; i++)
+//		{
+//			const char* name;
+//			{
+//				std::lock_guard lock(m_mutex);
+//				name = m_chestContents[i].data();
+//			}
+//
+//			if (strlen(name) == 0)
+//				continue;
+//
+//			POINT pos = m_slotToMousePosOffset[i];
+//
+//			pos.x += (int)middle.x;
+//			pos.y += (int)middle.y;
+//
+//			draw->AddText({ (float)pos.x, (float)pos.y }, IM_COL32(255, 0, 0, 255), name);
+//		}
+//
+//	}
+//}
 
 }
--- ToadClient/src/Toad/Modules/chest_stealer.h
+++ ToadClient/src/Toad/Modules/chest_stealer.h
@@ -7,18 +7,33 @@ class CChestStealer SET_MODULE_CLASS(CChestStealer)
 {
 public:
 	CChestStealer();
+
+public:
 	void PreUpdate() override;
 	void Update(const std::shared_ptr<LocalPlayer>& lPlayer) override;
-	void OnImGuiRender(ImDrawList * draw) override;
+	//void OnImGuiRender(ImDrawList * draw) override;
 
 private:
+	// sets up the path the mouse takes to grab items
+	// Will also filter items depending on settings
+	void SetupPath();
+
 	POINT get_middle_of_screen() const;
 
 private:
 	// offset is from the middle of the window
-	static inline std::array<POINT, 27> m_slotToMousePosOffset;
+	static inline std::array<POINT, 27> m_slotToMousePosOffset{};
 
-	// No randomization on these, because they are used for block hitting and not for clicking
+	// chest contents
+	std::array<std::string, 27> m_chestContents{};
+
+	// path for mouse, ends with -1
+	std::deque<int> m_indexPath{};
+
+	std::mutex m_mutex;
+
+	bool m_isShiftDown = false;
+
 	static inline void right_mouse_down(const POINT & pt);
 	static inline void right_mouse_up(const POINT & pt);
 };
